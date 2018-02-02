52db219d97a501724aa1b8e61be77c868fce82ce
Ugnius <ugnius.malukas@gmail.com>
2018-02-02 21:36:44 +0200

    Refactor code style.

--- et-auto.sh
+++ et-auto.sh
@@ -1,37 +1,39 @@
 #!/usr/bin/env bash
-# Regular Colors
-Black='\e[0;30m'        # Black
-Red='\e[0;31m'          # Red
-Green='\e[0;32m'        # Green
-Yellow='\e[0;33m'       # Yellow
-Blue='\e[0;34m'         # Blue
-Purple='\e[0;35m'       # Purple
-Cyan='\e[0;36m'         # Cyan
-White='\e[0;37m'        # White
 
-# Bold
-BBlack='\e[1;30m'       # Black
-BRed='\e[1;31m'         # Red
-BGreen='\e[1;32m'       # Green
-BYellow='\e[1;33m'      # Yellow
-BBlue='\e[1;34m'        # Blue
-BPurple='\e[1;35m'      # Purple
-BCyan='\e[1;36m'        # Cyan
-BWhite='\e[1;37m'       # White
+# Regular colors.
+BLACK='\e[0;30m'        # black
+RED='\e[0;31m'          # red
+GREEN='\e[0;32m'        # green
+YELLOW='\e[0;33m'       # yellow
+BLUE='\e[0;34m'         # blue
+PURPLE='\e[0;35m'       # purple
+CYAN='\e[0;36m'         # cyan
+WHITE='\e[0;37m'        # white
+
+# Bold colors.
+BOLD_BLACK='\e[1;30m'       # black
+BOLD_RED='\e[1;31m'         # red
+BOLD_GREEN='\e[1;32m'       # green
+BOLD_YELLOW='\e[1;33m'      # yellow
+BOLD_BLUE='\e[1;34m'        # blue
+BOLD_PURPLE='\e[1;35m'      # purple
+BOLD_CYAN='\e[1;36m'        # cyan
+BOLD_WHITE='\e[1;37m'       # white
 
 # Default
-DefaultColor='\e[39m'   # Default foreground color
+DEFAULT_FOREGROUND_COLOR='\e[39m'   # Default foreground color.
 
 echo -ne '\033c'
-echo -e ${Cyan}   "    +${Yellow}------------------------------------------------------------------${Cyan}+"
-echo -e ${Yellow}   "    |                                                                  |"
-echo -e "     |$Red    ███████╗████████╗    ${Green}█████╗ ██╗   ██╗████████╗ ██████╗ ${Purple}██╗   $Yellow |"
-echo -e "     |$Red    ██╔════╝╚══██╔══╝   ${Green}██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗${Purple}██║   $Yellow |"
-echo -e "     |$BRed    █████╗     ██║${White}█████╗${BGreen}███████║██║   ██║   ██║   ██║   ██║${BPurple}██║   $Yellow |"
-echo -e "     |$BRed    ██╔══╝     ██║${White}╚════╝${BGreen}██╔══██║██║   ██║   ██║   ██║   ██║${BPurple}╚═╝   $Yellow |"
-echo -e "     |$Red    ███████╗   ██║      ${Green}██║  ██║╚██████╔╝   ██║   ╚██████╔╝${Purple}██╗   $Yellow |"
-echo -e "     |$Red    ╚══════╝   ╚═╝      ${Green}╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ${Purple}╚═╝   $Yellow |"
-echo -e ${Yellow}   "    |                                                                  |"
-echo -e ${Cyan}   "    +${Yellow}------------------------------------------------------------------${Cyan}+${Yellow}"
-echo -e "                        |${BRed} Evil${BYellow} Twin${BPurple} Automated${BGreen} Attack${Yellow} |"
-echo -e "                        ${Cyan}+${Yellow}----------------------------${Cyan}+"
+echo -e ${CYAN}   "    +${YELLOW}------------------------------------------------------------------${CYAN}+"
+echo -e ${YELLOW}   "    |                                                                  |"
+echo -e "     |${RED}    ███████╗████████╗    ${GREEN}█████╗ ██╗   ██╗████████╗ ██████╗ ${PURPLE}██╗   ${YELLOW} |"
+echo -e "     |${RED}    ██╔════╝╚══██╔══╝   ${GREEN}██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗${PURPLE}██║   ${YELLOW} |"
+echo -e "     |${BOLD_RED}    █████╗     ██║${WHITE}█████╗${BOLD_GREEN}███████║██║   ██║   ██║   ██║   ██║${BOLD_PURPLE}██║   ${YELLOW} |"
+echo -e "     |${BOLD_RED}    ██╔══╝     ██║${WHITE}╚════╝${BOLD_GREEN}██╔══██║██║   ██║   ██║   ██║   ██║${BOLD_PURPLE}╚═╝   ${YELLOW} |"
+echo -e "     |${BOLD_RED}    ███████╗   ██║      ${GREEN}██║  ██║╚██████╔╝   ██║   ╚██████╔╝${PURPLE}██╗   ${YELLOW} |"
+echo -e "     |${BOLD_RED}    ╚══════╝   ╚═╝      ${GREEN}╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝ ${PURPLE}╚═╝   ${YELLOW} |"
+echo -e ${YELLOW}   "    |                                                                  |"
+echo -e ${CYAN}   "    +${YELLOW}------------------------------------------------------------------${CYAN}+${YELLOW}"
+echo -e "                        |${BOLD_RED} Evil${BOLD_YELLOW} Twin${BOLD_PURPLE} Automated${BOLD_GREEN} Attack${YELLOW} |"
+echo -e "                        ${CYAN}+${YELLOW}----------------------------${CYAN}+"
+
